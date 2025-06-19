07a8ec8dd99f95085b5ee0a55b25b12f224c4ce1
Jake Styles <jakegstyles@gmail.com>
2025-06-19 10:53:56 -0700

    added admin page

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/pass_overs");
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/scripts");
+    eleventyConfig.addPassthroughCopy("./src/admin");
 
     eleventyConfig.addFilter('showDate', (dateObj) =>{
         return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toLocaleString(DateTime.DATE_HUGE);
